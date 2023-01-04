790f629a53881c41fd556284b694cf23960c99af
CGranzell <91188714+CGranzell@users.noreply.github.com>
2023-01-04 06:34:29 +0100

    App klar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from './pages/Home';
 import Products from "./pages/Products";
+import Footer from "./layout/Footer";
+import Header from "./layout/Header";
 import Product from "./pages/Product";
-import Footer from './layout/Footer';
+import SearchResult from "./pages/SearchResult";
+import Home from "./pages/Home";
+import GlobalState from "./context/GlobalState";
+import Cart from "./pages/Cart";
+import ShopContext from "./context/ShopContext";
+
 
 function App() {
-  return (
-    <div>
-    <BrowserRouter>
+  
 
-     <Routes>
-     <Route path="/" element={<Home />} />
-     <Route path="/products" element={<Products />} />
-     <Route path="/products/:productId" element={<Product />} />
-     </Routes>
-     <Footer />
-    </BrowserRouter>
-    </div>
+
+  return (
+    <GlobalState>
+      <BrowserRouter>
+      <ShopContext.Consumer>
+        {context => (
+              <Header 
+            cartItemNumber={context.cart.reduce((count, curItem) => {
+              return count + curItem.quantity;
+            }, 0)}
+          />
+        )}
+      </ShopContext.Consumer>
+           
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/products/:productId" element={<Product />} />
+          <Route path="/searchresult/:searchTerm" element={<SearchResult />} />
+          <Route path="/cart" element={<Cart />}/>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </GlobalState>
   );
 }
 
